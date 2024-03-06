9f91e1afb2eb6213b885ae78ef1375bf7cbcdc6b
yxhwxn <officeyh9819@gmail.com>
2024-03-06 23:27:58 +0900

    백준 2557번

--- backjoon/basic/10869.py
+++
@@ -1,8 +0,0 @@
-user_input = input("두 수를 입력하시오(공백으로 구분) : ")
-num1, num2 = map(int, user_input.split())
-
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 // num2)
-print(num1 % num2)
