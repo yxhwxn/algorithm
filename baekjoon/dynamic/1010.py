623e906766c0fa08e02d3421791bf1bcfc1bd554
yxhwxn <officeyh9819@gmail.com>
2024-04-07 00:09:15 +0900

    백준 1010.py

---
+++ baekjoon/dynamic/1010.py
@@ -0,0 +1,25 @@
+# 방법 1 (factorial 직접 구현)
+def factorial(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
+
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    bridge = factorial(m) // (factorial(n) * factorial(m - n))
+    print(bridge)
+
+''' 방법 2
+import math
+
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    bridge = math.factorial(m) // (math.factorial(n) * math.factorial(m - n))
+    print(bridge)
+'''
+
