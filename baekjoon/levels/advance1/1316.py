2bbd364ae22ef635ee0f5c2a19066d4e7335a44f
yxhwxn <officeyh9819@gmail.com>
2024-04-14 21:21:52 +0900

    백준 1316번(그룹 단어 체커)

---
+++ baekjoon/levels/advance1/1316.py
@@ -0,0 +1,15 @@
+# 그룹 단어 체커
+
+N = int(input())
+cnt = N
+
+for i in range(N):
+    word = input()
+    for j in range(0, len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+
+print(cnt)
