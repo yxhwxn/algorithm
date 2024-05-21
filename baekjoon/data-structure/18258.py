6d062c5b73a0d0d200bfcc251079ca5c799c2dec
yxhwxn <officeyh9819@gmail.com>
2024-05-21 23:53:24 +0900

    백준 18258번

---
+++ baekjoon/data-structure/18258.py
@@ -0,0 +1,62 @@
+# 초기버전(시간초과 됨)
+import sys
+n = int(input())
+queue = []
+for i in range(n):
+    com = sys.stdin.readline().split()
+    if com[0] == 'push':
+        queue.append(com[1])
+    elif com[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.pop(0))  
+    elif com[0] == 'size':
+        print(len(queue))
+    elif com[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif com[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif com[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
+
+# deque로 개선
+import sys
+from collections import deque
+n = int(input())
+queue = deque([])
+for i in range(n):
+    com = sys.stdin.readline().split()
+    if com[0] == 'push':
+        queue.append(com[1])
+    elif com[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif com[0] == 'size':
+        print(len(queue))
+    elif com[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif com[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif com[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
